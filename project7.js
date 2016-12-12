82cf93e97b7b49766dda720cd2f4d80c0ba1bb21
max sami <skull037037@gmail.com>
2016-12-12 10:25:42 -0800

    Parts 1, 2, and 3
    added draw rect, draw circle and the mouse events

--- project7.js
+++ project7.js
@@ -11,12 +11,33 @@ function transformPoint(event) {
 }
 
 // Step 2: drawSquare and drawCircle functions
-function drawSquare(x, y, size, color) {
-  // square drawing code here
+function drawRectangle(width, height, color) {
+    var newrect = document.createElementNS(namespace, "rect")
+  newrect.setAttribute("x",   pt.x)
+  newrect.setAttribute("y",   pt.y)
+  newrect.setAttribute("width", width)
+  newrect.setAttribute("height", height)
+  newrect.setAttribute("fill", color)
+  screen.appendChild(newrect)
 }
+
+function drawCircle(color,r) {
+  var newcircle = document.createElementNS(namespace, "circle")
+  newcircle.setAttribute("cx", pt.x)
+  newcircle.setAttribute("cy",pt.y)
+  newcircle.setAttribute("fill", color)
+  newcircle.setAttribute("r", r)
+  screen.appendChild(newcircle);
+}
+
 
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
-  // what do you want to do when the user presses down
-  // on the mouse button?
+
+})
+document.addEventListener("mouseup", function(e) {
+
+})
+document.addEventListener("mousemove", function(e) {
+
 })
